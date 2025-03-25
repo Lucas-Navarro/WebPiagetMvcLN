ca6c6e02e58182a492935a126bc21f72ebf19dbe
Lucas-Navarro <lucasr6out@gmail.com>
2025-03-25 11:12:08 -0300

    Upando

--- WebPiagetMvcLN/Data/ApplicationDbContext.cs
+++ WebPiagetMvcLN/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebPiagetMvcLN.Models;
 
 namespace WebPiagetMvcLN.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+public DbSet<WebPiagetMvcLN.Models.Aluno> Aluno { get; set; } = default!;
 }
